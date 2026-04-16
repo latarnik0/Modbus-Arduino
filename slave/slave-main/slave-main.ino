96e095c17fa1c903b60f50b40dcb2b0444476cac
latarnik0 <154380708+latarnik0@users.noreply.github.com>
2026-04-16 13:05:06 +0200

    Refactor Modbus communication setup and handling

--- slave/slave-main/slave-main.ino
+++ slave/slave-main/slave-main.ino
@@ -1,25 +1,42 @@
 #include <ModbusRTU.h>
 
-#define TX_DIR_PIN 2 // pin sterujący kierunkiem transmisji
-#define SLAVE_ID 1
+//KONFIGURACJA MODBUS
+#define SLAVE_ID 1        
+#define TXEN_PIN 2        // pin sterujący nadawaniem/odbiorem
 
+ModbusRTU mb;
 
-ModbusRTU m;
+//PINY SPRZĘTOWE
+#define LED_PIN 13        // Pin diody LED do sterowania przez Mastera
+#define SENSOR_PIN A0     // Analogowy pin dla przykładowego czujnika
 
 void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+
   Serial.begin(9600);
 
-  // przypisanie portu szeregowego i pinu sterującego
-  m.begin(&Serial, TX_DIR_PIN);
+  mb.begin(&Serial, TXEN_PIN);  
+  mb.slave(SLAVE_ID);          
 
-  // Ustawienie id na 1
-  m.slave(SLAVE_ID);
-
-  // adres i wartość rejestru. Początkowa wartość na 0
-  m.addHreg(0, 0);
+  // REJESTRACJA REJESTRÓW
+  mb.addHreg(0, 0); // Rejestr 0: Czujnik analogowy 
+  mb.addHreg(1, 0); // Rejestr 1: Uptime 
+  mb.addHreg(2, 0); // Rejestr 2: Sterowanie LED 
 }
 
 void loop() {
-  // funkcja nasłuchująca zapytań od mastera 
+  //OBSŁUGA KOMUNIKACJI
   mb.task();
+
+  // zapis wartości do wewnętrznych rejestrów Modbus
+  mb.Hreg(0, analogRead(SENSOR_PIN));
+  mb.Hreg(1, millis() / 1000);
+
+  // REAKCJA NA KOMENDY
+  if (mb.Hreg(2) > 0) {
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    digitalWrite(LED_PIN, LOW);
+  }
 }
